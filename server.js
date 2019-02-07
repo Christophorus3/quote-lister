05205e1004fc380aab16739e210a26793aa01ef1
christophwottawa <christoph.wottawa@me.com>
2019-02-07 18:57:27 +0100

    added mini server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const favicon = require('express-favicon')
+const path = require('path')
+
+
+const port = process.env.port || 8080
+
+const app = express()
+
+//app.use(favicon(__dirname + "/build/favicon.ico"))
+
+//app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, "build")))
+
+// ping pong route - for test only.
+app.get('/ping', function (req, res) {
+  return res.send("pong")
+})
+
+// serve the react app in index.html
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
+app.listen(port)
