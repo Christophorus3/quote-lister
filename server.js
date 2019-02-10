b51b746a6700d6acc3a08d1e72bde2cbda77c7aa
christophwottawa <christoph.wottawa@me.com>
2019-02-10 11:49:28 +0100

    connecting to mongodb

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const favicon = require('express-favicon')
 const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
+const MongoClient = require('mongodb').MongoClient
 
+// prepare MongoClient
+const mongoUser = process.env.MONGO_USER
+const mongoPwd = process.env.MONGO_PWD
+const mongoDbName = process.env.MONGO_DB
+const mongoUrl = `mongodb://${mongoUser}:${mongoPwd}@wottawa.at:27017/${mongoDbName}`
+
+let db //holds mongo database
 
 const port = process.env.port || 8080
 
@@ -27,8 +37,11 @@ app.get('/quote', (req, res) => {
 })
 
 app.post('/quote', (req, res) => {
-  console.log(req.body)
+  const {name, quote} = req.body
+  console.log("name: ", name)
+  console.log("quote: ", quote)
   return res.sendStatus(200)
+  //return res.redirect('/')
 })
 
 // serve the react app in index.html
@@ -36,5 +49,15 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 
+MongoClient.connect(mongoUrl, (err, client) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log("connected to mongodb")
+    db = client.db("quotes")
 
-app.listen(port)
+    app.listen(port, () => {
+      console.log("quote api running on port " + port)
+    })
+  }
+})
